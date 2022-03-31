a0f91031635e0da4c2279bc44b26413d00eccf6b
Kyle Hodgetts <kyle@kubeshop.io>
2022-03-31 16:46:55 +0100

    go fmt

--- server/go/api.go
+++ server/go/api.go
@@ -14,60 +14,57 @@ import (
 	"net/http"
 )
 
-
-
 // ApisApiRouter defines the required methods for binding the api requests to a responses for the ApisApi
 // The ApisApiRouter implementation should parse necessary information from the http request,
 // pass the data to a ApisApiServicer to perform the required actions, then write the service results to the http response.
-type ApisApiRouter interface { 
+type ApisApiRouter interface {
 	GetApi(http.ResponseWriter, *http.Request)
 	GetApis(http.ResponseWriter, *http.Request)
 	GetPostProcessedOpenApiSpec(http.ResponseWriter, *http.Request)
 	GetRawOpenApiSpec(http.ResponseWriter, *http.Request)
 }
+
 // FleetsApiRouter defines the required methods for binding the api requests to a responses for the FleetsApi
 // The FleetsApiRouter implementation should parse necessary information from the http request,
 // pass the data to a FleetsApiServicer to perform the required actions, then write the service results to the http response.
-type FleetsApiRouter interface { 
+type FleetsApiRouter interface {
 	GetEnvoyFleet(http.ResponseWriter, *http.Request)
 	GetEnvoyFleets(http.ResponseWriter, *http.Request)
 }
+
 // ServicesApiRouter defines the required methods for binding the api requests to a responses for the ServicesApi
 // The ServicesApiRouter implementation should parse necessary information from the http request,
 // pass the data to a ServicesApiServicer to perform the required actions, then write the service results to the http response.
-type ServicesApiRouter interface { 
+type ServicesApiRouter interface {
 	GetService(http.ResponseWriter, *http.Request)
 	GetServices(http.ResponseWriter, *http.Request)
 }
-
 
 // ApisApiServicer defines the api actions for the ApisApi service
 // This interface intended to stay up to date with the openapi yaml used to generate it,
 // while the service implementation can ignored with the .openapi-generator-ignore file
 // and updated with the logic required for the API.
-type ApisApiServicer interface { 
+type ApisApiServicer interface {
 	GetApi(context.Context, string, string) (ImplResponse, error)
 	GetApis(context.Context, string, string) (ImplResponse, error)
 	GetPostProcessedOpenApiSpec(context.Context, string, string) (ImplResponse, error)
 	GetRawOpenApiSpec(context.Context, string, string) (ImplResponse, error)
 }
 
-
 // FleetsApiServicer defines the api actions for the FleetsApi service
 // This interface intended to stay up to date with the openapi yaml used to generate it,
 // while the service implementation can ignored with the .openapi-generator-ignore file
 // and updated with the logic required for the API.
-type FleetsApiServicer interface { 
+type FleetsApiServicer interface {
 	GetEnvoyFleet(context.Context, string, string) (ImplResponse, error)
 	GetEnvoyFleets(context.Context, string) (ImplResponse, error)
 }
-
 
 // ServicesApiServicer defines the api actions for the ServicesApi service
 // This interface intended to stay up to date with the openapi yaml used to generate it,
 // while the service implementation can ignored with the .openapi-generator-ignore file
 // and updated with the logic required for the API.
-type ServicesApiServicer interface { 
+type ServicesApiServicer interface {
 	GetService(context.Context, string, string) (ImplResponse, error)
 	GetServices(context.Context, string) (ImplResponse, error)
 }
