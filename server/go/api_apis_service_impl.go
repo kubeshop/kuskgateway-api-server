69e91833ba582f730ce88f92e03a4d4210b9ccb7
Jasmin Gacic <jasmin.gacic@gmail.com>
2022-04-07 13:03:00 +0000

    fix return value for POST /apis
    Signed-off-by: Jasmin Gacic <jasmin.gacic@gmail.com>

--- server/go/api_apis_service_impl.go
+++ server/go/api_apis_service_impl.go
@@ -35,14 +35,14 @@ func NewApisApiService(kuskClient kusk.Client) ApisApiServicer {
 
 // GetApis - Get a list of APIs
 func (s *ApisApiService) GetApis(ctx context.Context, namespace string, fleetname string, fleetnamespace string) (ImplResponse, error) {
-	apis := &kuskv1.APIList{}
+	var apis *kuskv1.APIList
 	var err error
+
 	if fleetname == "" {
 		apis, err = s.kuskClient.GetApis(namespace)
 		if err != nil {
 			return Response(http.StatusInternalServerError, err), err
 		}
-
 	} else {
 		apis, err = s.kuskClient.GetApiByEnvoyFleet(namespace, fleetnamespace, fleetname)
 		if err != nil {
@@ -130,4 +130,14 @@ func (s *ApisApiService) convertAPICRDtoAPIModel(api *kuskv1.API) ApiItem {
 	}
 
 	return apiItem
+}
+
+// DeployApi - Deploy new API
+func (s *ApisApiService) DeployApi(ctx context.Context, payload APIPayload) (ImplResponse, error) {
+	api, err := s.kuskClient.CreateApi(payload.Name, payload.Namespace, payload.Openapi)
+	if err != nil {
+		return Response(http.StatusInternalServerError, err), err
+	}
+
+	return Response(http.StatusOK, s.convertAPICRDtoAPIModel(api)), nil
 }
